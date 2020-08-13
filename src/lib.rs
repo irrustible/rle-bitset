5c019edfce71e070bd8ff8356942b1b98ba77744
James Laver <james.laver@gmail.com>
2020-08-13 08:25:24 +0200

    first tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
-//! A no-std compatible trait for querying and manipulating bits in a
+//! A no-std, no-alloc trait for querying and manipulating bits in a
 //! [`[usize]`] and iterating their run lengths.
-#![no_std]
-
-extern crate alloc;
-
+// #![no_std]
 use core::cmp::min;
 use core::ops::{Bound::*, Range, RangeBounds};
 use primitive_traits::Integer;
 
+pub const WORD_WIDTH: usize = <usize as Integer>::WIDTH;
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct OutOfRange();
 
-trait RLEBits {
+pub trait RLEBits {
+    /// Gets the bit at the provided index, if it is within range.
     fn get_bit(&self, bit: usize) -> Result<bool, OutOfRange>;
+    /// Sets the bit at the provided index, if it is within range.
     fn set_bit(&mut self, bit: usize, value: bool) -> Result<(), OutOfRange>;
+    /// Returns an iterator over run lengths of the bits within the provided range.
     fn run_lengths<'a, R: RangeBounds<usize>>(&'a self, range: R) -> Result<RLE<'a>, OutOfRange>;
 }
 
@@ -38,16 +40,15 @@ impl RLEBits for [usize] {
     }
 }
 
-const WIDTH: usize = <usize as Integer>::WIDTH;
-
 fn locate(slice: &[usize], bit: usize) -> Result<(usize, usize), OutOfRange> {
-    if bit < (WIDTH * slice.len()) {
-        Ok((bit / WIDTH, bit % WIDTH))
+    if bit < (WORD_WIDTH * slice.len()) {
+        Ok((bit / WORD_WIDTH, bit % WORD_WIDTH))
     } else {
         Err(OutOfRange())
     }
 }
 
+/// A run length - a range of the same value repeated.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct RL {
     pub value: bool,
@@ -55,21 +56,24 @@ pub struct RL {
 }
 
 impl RL {
-    fn new(value: bool, start: usize, end: usize) -> RL {
+    /// Creates a new [`RL`].
+    pub fn new(value: bool, start: usize, end: usize) -> RL {
         RL { value, run: start..end }
     }
 }
 
+/// An iterator over run lengths of bits.
+#[derive(Debug)]
 pub struct RLE<'a> {
     storage: &'a [usize],
-    range: Range<usize>,
+    pub range: Range<usize>,
     last: Option<RL>,
 }
 
 impl<'a> RLE<'a> {
 
     pub fn new<R: RangeBounds<usize>>(storage: &'a [usize], range: R) -> Result<RLE<'a>, OutOfRange> {
-        let size = storage.len() * WIDTH;
+        let size = storage.len() * WORD_WIDTH;
         let s = match range.start_bound() {
             Included(x) => if *x < size { Ok(*x) } else { Err(OutOfRange()) },
             Excluded(x) => if (*x+1) < size { Ok(*x+1) } else { Err(OutOfRange()) },
@@ -83,14 +87,16 @@ impl<'a> RLE<'a> {
         if e >= s { Ok(RLE { storage, range: s..e, last: None }) } else { Err(OutOfRange()) }
     }
 
+    /// If we are within the specified range, return the position to start
     fn start_run(&self) -> Option<(usize, bool)> {
         if let Some(last) = &self.last {
             if last.run.end < self.range.end { Some((last.run.end, !last.value)) } else { None }
         } else {
-            if self.range.start < self.range.end { Some((0, 1 == (self.storage[0] & 1))) } else { None }
+            if self.range.start < self.range.end { Some((self.range.start, 1 == (self.storage[0] & 1))) } else { None }
         }
     }
 
+    // Get the current block in a manner ready for `.trailing_zeros()`.
     fn block(&self, block: usize, of: bool) -> usize {
         if of { !self.storage[block] } else { self.storage[block] }
     }
@@ -100,22 +106,22 @@ impl<'a> Iterator for RLE<'a> {
     type Item = RL;
     fn next(&mut self) -> Option<Self::Item> {
         let (start, of) = self.start_run()?;
-        let x = start / WIDTH;
-        let y = start % WIDTH;
-        let z = WIDTH - y;
+        let x = start / WORD_WIDTH;
+        let y = start % WORD_WIDTH;
+        let bits_left = WORD_WIDTH - y; // in the block
         let block = self.block(x, of) >> y;
-        let len = min(block.trailing_zeros() as usize, z);
-        let end = start + len;
-        let ret = if end < self.range.end  {
-            if len == z { // might continue into the next block!
-                let extra = self.block(x+1, of).leading_zeros() as usize;
-                RL::new(of, start, min(end + extra, self.range.end))
-            } else {
-                RL::new(of, start, end)
+        let len = min(block.trailing_zeros() as usize, bits_left);
+        let mut end = start + len;
+        if len == bits_left {
+            let mut x = x + 1;
+            while end < self.range.end {
+                let extra = self.block(x, of).trailing_zeros() as usize;
+                end += extra;
+                if extra != WORD_WIDTH { break; }
+                x += 1;
             }
-        } else {
-            RL::new(of, start, self.range.end)
-        };
+        }
+        let ret = RL::new(of, start, min(end, self.range.end));
         self.last = Some(ret.clone());
         Some(ret)
     }
---
+++ tests/test.rs
@@ -0,0 +1,49 @@
+#![feature(test)]
+
+use rle_bitset::*;
+
+#[test]
+fn empty() {
+    let mut x: [usize; 4] = [0, 0, 0, 0];
+    let over = WORD_WIDTH * 4;
+    for i in 0..over {
+        assert_eq!(x.get_bit(i).unwrap(), false);
+    }
+    assert!(x.get_bit(over).is_err());
+    x.set_bit(WORD_WIDTH, false).unwrap();
+    for i in 0..over {
+        assert_eq!(x.get_bit(i).unwrap(), false);
+    }
+    {
+        let mut iter = x.run_lengths(..).unwrap();
+        assert_eq!(iter.range, 0..over);
+        assert_eq!(Some(RL::new(false, 0, over)), iter.next());
+        assert_eq!(None, iter.next());
+    }
+    assert!(x.get_bit(over).is_err());
+}
+#[test]
+fn one() {
+    let mut x: [usize; 4] = [0, 0, 0, 0];
+    let over = WORD_WIDTH * 4;
+    x.set_bit(WORD_WIDTH, true).unwrap();
+    assert_eq!(x.get_bit(WORD_WIDTH).unwrap(), true);
+    {
+        let mut iter = x.run_lengths(..).unwrap();
+        assert_eq!(iter.range, 0..over);
+        assert_eq!(Some(RL::new(false, 0, WORD_WIDTH)), iter.next());
+        assert_eq!(Some(RL::new(true, WORD_WIDTH, WORD_WIDTH + 1)), iter.next());
+        assert_eq!(Some(RL::new(false, WORD_WIDTH + 1, over)), iter.next());
+        assert_eq!(None, iter.next());
+    }
+    x.set_bit(WORD_WIDTH - 1, true).unwrap();
+    assert_eq!(x.get_bit(WORD_WIDTH - 1).unwrap(), true);
+    {
+        let mut iter = x.run_lengths(..).unwrap();
+        assert_eq!(iter.range, 0..over);
+        assert_eq!(Some(RL::new(false, 0, WORD_WIDTH - 1)), iter.next());
+        assert_eq!(Some(RL::new(true, WORD_WIDTH -1, WORD_WIDTH + 1)), iter.next());
+        assert_eq!(Some(RL::new(false, WORD_WIDTH + 1, over)), iter.next());
+        assert_eq!(None, iter.next());
+    }
+}
